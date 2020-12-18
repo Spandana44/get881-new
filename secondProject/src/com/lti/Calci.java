1ebc43574636f47826db1fc31b40d93b1334f1bb
Spandana44 <76201823+Spandana44@users.noreply.github.com>
2020-12-18 16:46:42 +0530

    Update Calci.java
    Added Product method

--- secondProject/src/com/lti/Calci.java
+++ secondProject/src/com/lti/Calci.java
@@ -6,4 +6,7 @@ public void add(int num1 , int num2) {
 	System.out.println("sum = " + (num1 + num2));
 }
 
+	public void product(int num1 , int num2) {
+	System.out.println("product = " + (num1 * num2));
+}
 }
