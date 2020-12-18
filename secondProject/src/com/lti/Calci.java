fca2eb955c67947dad0f30fd549f1c04305bf500
Spandana44 <reddyspandana4444@gmail.com>
2020-12-18 16:41:47 +0530

    Modified Calci class

--- secondProject/src/com/lti/Calci.java
+++ secondProject/src/com/lti/Calci.java
@@ -1,8 +1,12 @@
 package com.lti;
 
 public class Calci {
+	
 public void add(int num1 , int num2) {
 	System.out.println("sum = " + (num1 + num2));
 }
 
+public void subs(int num1 , int num2) {
+	System.out.println("difference = " + (num1 - num2));
+}
 }
